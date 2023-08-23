6008489802525107bc37f37c5fb373c46dc66099
vivienkp <vivienkp@gmail.com>
2023-08-22 23:13:45 -0400

    Set up restaurant menu pages and components

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -5,6 +5,7 @@ import { getUser } from '../../utilities/users-service';
 import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+import RestaurantSearchPage from '../RestaurantSearchPage/RestaurantSearchPage';
 
 export default function App() {
   const [user, setUser] = useState(getUser());
@@ -15,6 +16,7 @@ export default function App() {
           {/* client-side route that renders the component instance if the path matches the url in the address bar */}
           <Route path="/orders/new" element={<NewOrderPage user={user} setUser={setUser} />} />
           <Route path="/orders" element={<OrderHistoryPage />} />
+          <Route path="/restaurants" element={<RestaurantSearchPage />} /> 
           {/* redirect to /orders/new if path in address bar hasn't matched a <Route> above */}
           <Route path="/*" element={<Navigate to="/orders/new" />} />
         </Routes>
