2e9cefc13cb20cd52347a5b7484069b3d9c5e7f2
corydesmond1 <corydesmond14@gmail.com>
2023-08-26 13:35:37 -0400

    better routing for backend response

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
 const cors = require("cors");
 // Always require and configure near the top
-require('dotenv').config();
+require("dotenv").config();
 // Connect to the database
-require('./config/database');
+require("./config/database");
 
 const app = express();
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(
   cors({
@@ -19,71 +18,33 @@ app.use(
   })
 );
 
-
 // Configure both serve-favicon & static middleware
 // to serve from the production 'build' folder
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
+app.use(express.static(path.join(__dirname, "build")));
 
 // Middleware to check and verify a JWT and
 // assign the user object from the JWT to req.user
-app.use(require('./config/checkToken'));
+app.use(require("./config/checkToken"));
 
 const port = process.env.PORT || 3001;
 
 // Put API routes here, before the "catch all" route
-app.use('/api/users', require('./routes/api/users'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/yelp", require("./routes/api/yelp"));
+
 
 // Protect all routes below from anonymous users
-const ensureLoggedIn = require('./config/ensureLoggedIn');
-app.use('/api/items', ensureLoggedIn, require('./routes/api/items'));
-app.use('/api/orders', ensureLoggedIn, require('./routes/api/orders'));
-
-
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    "x-requested-with": "xmlhttprequest",
-    "Access-Control-Allow-Origin": "*",
-    Authorization: "Bearer " + process.env.REACT_APP_YELP,
-  },
-};
-
-async function yelpBusinessDetail(businessId) {
-  return await fetch(
-    `https://api.yelp.com/v3/businesses/${businessId}`,
-    options
-  ).then((response) => response.json());
-}
-
-app.get("/restaurants/:id", async function (req, res) {
-  const businessId = req.params.id;
-  const data = await yelpBusinessDetail(businessId);
-  res.send(data);
-});
-
-async function yelpBusinessSearch(term, location) {
-  return await fetch(
-    `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}`,
-    options
-  ).then((response) => response.json());
-}
-
-app.get("/restaurants", async function (req, res) {
-  const term = req.query.term;
-  const location = req.query.location;
-  console.log(term, location);
-  const data = await yelpBusinessSearch(term, location);
-  res.send(data);
-});
+const ensureLoggedIn = require("./config/ensureLoggedIn");
+app.use("/api/items", ensureLoggedIn, require("./routes/api/items"));
+app.use("/api/orders", ensureLoggedIn, require("./routes/api/orders"));
 
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX/API requests
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Express app running on port ${port}`);
 });
--- src/utilities/yelp-api.js
+++ src/utilities/yelp-api.js
@@ -1,25 +1,9 @@
-// import yelp from 'yelp-fusion';
-// const client = yelp.client(process.env.REACT_APP_YELP);
-
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    "x-requested-with": "xmlhttprequest",
-    "Access-Control-Allow-Origin": "*",
-    Authorization: "Bearer " + process.env.REACT_APP_YELP,
-  },
-   success: function(result){
-        console.log(result);
-    }
- };
-
 export async function yelpBusinessSearch(term, location) {
   return await fetch(
-    `http://localhost:3001/restaurants?term=${term}&location=${location}`
+    `http://localhost:3001/api/yelp/restaurants?term=${term}&location=${location}`
   ).then((res) => res.json());
 };
 
  export async function yelpBusinessDetail(businessId) {
-   return await fetch(`http://localhost:3001/restaurants/${businessId}`).then((res) => res.json());
+   return await fetch(`http://localhost:3001/api/yelp/restaurants/${businessId}`).then((res) => res.json());
 };
