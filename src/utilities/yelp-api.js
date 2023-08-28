82ac60243f237c51000e2f6626432fcc64181771
vivienkp <vivienkp@gmail.com>
2023-08-28 16:19:43 -0400

    rerouted yelp API to heroku instead of localhost

--- src/utilities/yelp-api.js
+++ src/utilities/yelp-api.js
@@ -1,9 +1,9 @@
 export async function yelpBusinessSearch(term, location) {
   return await fetch(
-    `http://localhost:3001/api/yelp/restaurants?term=${term}&location=${location}`
+    `https://culinarydash-1e778c071eab.herokuapp.com/api/yelp/restaurants?term=${term}&location=${location}`
   ).then((res) => res.json());
 };
 
  export async function yelpBusinessDetail(businessId) {
-   return await fetch(`http://localhost:3001/api/yelp/restaurants/${businessId}`).then((res) => res.json());
+   return await fetch(`https://culinarydash-1e778c071eab.herokuapp.com/api/yelp/restaurants/${businessId}`).then((res) => res.json());
 };
