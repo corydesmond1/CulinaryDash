8b2f88ae8a2d55dea9c5396fedfe4efa8d70dfd1
vivienkp <vivienkp@gmail.com>
2023-08-28 16:26:32 -0400

    adjusted yelp.api

--- src/utilities/yelp-api.js
+++ src/utilities/yelp-api.js
@@ -1,9 +1,9 @@
 export async function yelpBusinessSearch(term, location) {
   return await fetch(
-    `https://culinarydash-1e778c071eab.herokuapp.com/api/yelp/restaurants?term=${term}&location=${location}`
+    `/api/yelp/restaurants?term=${term}&location=${location}`
   ).then((res) => res.json());
 };
 
  export async function yelpBusinessDetail(businessId) {
-   return await fetch(`https://culinarydash-1e778c071eab.herokuapp.com/api/yelp/restaurants/${businessId}`).then((res) => res.json());
+   return await fetch(`/api/yelp/restaurants/${businessId}`).then((res) => res.json());
 };
