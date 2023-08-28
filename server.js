2e7630680e8552a2eaa762da0889c22a4b9c2b33
vivienkp <vivienkp@gmail.com>
2023-08-28 15:28:41 -0400

    added heroku url to backend server

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://culinarydash-1e778c071eab.herokuapp.com/"]
   })
 );
 
